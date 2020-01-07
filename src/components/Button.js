38521becf2110ec5cfd67184e2ccfec72f81ffc2
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-07 13:56:05 -0300

    fix: change button wide prop type to boolean

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, color, wide }) => (
-  <div className="button" style={{ backgroundColor: color, flex: wide }}>{name}</div>
-);
+const Button = ({ name, color, wide }) => {
+  const style = { backgroundColor: color };
+  if (wide) style.flex = 2;
+  return <div className="button" style={style}>{name}</div>;
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   color: PropTypes.string,
-  wide: PropTypes.number,
+  wide: PropTypes.bool,
 };
 
 Button.defaultProps = {
   color: 'orange',
-  wide: 1,
+  wide: false,
 };
 
 export default Button;
